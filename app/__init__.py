f4efb5df198dd0acfaf0499614bff4d4388ad7cf
Midas24kMidas24k <137853877+Midas24k@users.noreply.github.com>
2024-07-03 21:37:40 -0400

    added create app function

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+def create_app(test_config=None):
+    app = Flask(__name__, static_url_path='/')
+    app.url_map.strict_slashes = False
+    app.config.from_mapping(
+        SECRET_Key='super_secret_key'
+    )
+
+    return app
