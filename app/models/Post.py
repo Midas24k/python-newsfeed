bbeee13052f70bd21a7a6b8b3df2c9cdd9e027e1
Midas24kMidas24k <137853877+Midas24k@users.noreply.github.com>
2024-07-11 10:41:34 -0400

    added votes model

--- app/models/Post.py
+++ app/models/Post.py
@@ -1,8 +1,8 @@
 from datetime import datetime
-from app.db import Base 
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
-from sqlalchemy.orm import relationship
-
+from app.db import Base
+from .Vote import Vote
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, select, func
+from sqlalchemy.orm import relationship, column_property
 
 class Post(Base):
     __tablename__ = 'posts'
@@ -13,6 +13,8 @@ class Post(Base):
     created_at = Column(DateTime, default=datetime.now)
     updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
     user = relationship('User')
-
     comments = relationship('Comment', cascade='all,delete')
-  
+    vote_count = column_property(
+        select(func.count(Vote.id)).where(Vote.post_id == id).scalar_subquery()
+    )
+    votes = relationship('Vote', cascade='all,delete')
