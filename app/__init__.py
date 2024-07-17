c4bb9794bcd37e36f189d1777dd7fcdbb1dae661
Midas24kMidas24k <137853877+Midas24k@users.noreply.github.com>
2024-07-17 19:36:01 -0400

    WIP: 33337c2 added filters.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from app.routes import home
 from app.routes import home, dashboard
 from app.db import init_db
+from app.utils import filters
+
 
 def create_app(test_config=None):
     app = Flask(__name__, static_url_path='/')
@@ -9,6 +11,7 @@ def create_app(test_config=None):
     app.config.from_mapping(
         SECRET_Key='super_secret_key'
     )
+
 
     @app.route('/hello')
     def hello():
@@ -20,4 +23,9 @@ def create_app(test_config=None):
     # initialize database
     init_db(app)
 
+    
+    app.jinja_env.filters['format_url'] = filters.format_url
+    app.jinja_env.filters['format_date'] = filters.format_date
+    app.jinja_env.filters['format_plural'] = filters.format_plural
+    
     return app
