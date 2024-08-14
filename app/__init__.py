df79a4bb88631d40492bb2c561840a44ad19a21f
Midas24kMidas24k <137853877+Midas24k@users.noreply.github.com>
2024-08-13 22:44:24 -0400

    Fixed bug with filters and finished routes

--- app/__init__.py
+++ app/__init__.py
@@ -5,17 +5,16 @@ from app.db import init_db
 from app.utils import filters
 
 
-def create_app(test_config=None):
+def create_app():
     app = Flask(__name__, static_url_path='/')
     app.url_map.strict_slashes = False
+    app.jinja_env.filters['format_url'] = filters.format_url
+    app.jinja_env.filters['format_date'] = filters.format_date
+    app.jinja_env.filters['format_plural'] = filters.format_plural
     app.config.from_mapping(
         SECRET_KEY='super_secret_key'
     )
 
-
-    @app.route('/hello')
-    def hello():
-        return 'hello, world!'
     # register routes
     app.register_blueprint(home) 
     app.register_blueprint(dashboard)
@@ -24,9 +23,5 @@ def create_app(test_config=None):
     # initialize database
     init_db(app)
 
-    
-    app.jinja_env.filters['format_url'] = filters.format_url
-    app.jinja_env.filters['format_date'] = filters.format_date
-    app.jinja_env.filters['format_plural'] = filters.format_plural
     
     return app
